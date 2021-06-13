2ea11c06f0896d4c2f7c6d96b00e86ed4c526651
Splitwirez <Splitwirez@gmail.com>
2021-06-13 16:38:25 -0400

    Window-related improvements

--- src/AvaloniaPlexTheme/Ctrl/WindowTitleBar.cs
+++ src/AvaloniaPlexTheme/Ctrl/WindowTitleBar.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 using System.Reactive.Linq;
 using Avalonia.Controls.Generators;
 using System.Collections;
+using Avalonia.Controls.Chrome;
 
 namespace AvaloniaPlexTheme
 {
@@ -22,15 +23,55 @@ namespace AvaloniaPlexTheme
         public const string TitleEmphasisStart = "[b]";
         public const string TitleEmphasisEnd = "[/b]";
 
-        static bool DefaultToLeftSideButtons => RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-        public static readonly StyledProperty<bool> LeftSideButtonsProperty =
-            AvaloniaProperty.Register<WindowTitleBar, bool>(nameof(LeftSideButtons), defaultValue: DefaultToLeftSideButtons);
         
-        public bool LeftSideButtons
+        //condition ? condition==true : condition==false
+        static HorizontalAlignment DefaultCaptionButtonsAlignment => RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? HorizontalAlignment.Left : HorizontalAlignment.Right;
+        public static readonly StyledProperty<HorizontalAlignment> DesiredCaptionButtonsAlignmentProperty =
+            AvaloniaProperty.Register<WindowTitleBar, HorizontalAlignment>(nameof(DesiredCaptionButtonsAlignment), defaultValue: DefaultCaptionButtonsAlignment);
+        
+        public HorizontalAlignment DesiredCaptionButtonsAlignment
         {
-            get => GetValue(LeftSideButtonsProperty);
-            set => SetValue(LeftSideButtonsProperty, value);
+            get => GetValue(DesiredCaptionButtonsAlignmentProperty);
+            set => SetValue(DesiredCaptionButtonsAlignmentProperty, value);
         }
+
+
+
+        public static readonly StyledProperty<CaptionButtons> WindowCaptionButtonsProperty =
+            AvaloniaProperty.Register<WindowTitleBar, CaptionButtons>(nameof(WindowCaptionButtons), defaultValue: null);
+        
+        public CaptionButtons WindowCaptionButtons
+        {
+            get => GetValue(WindowCaptionButtonsProperty);
+            set => SetValue(WindowCaptionButtonsProperty, value);
+        }
+
+
+
+        public static readonly AttachedProperty<double> LeftCaptionButtonsWidthProperty =
+            AvaloniaProperty.RegisterAttached<WindowTitleBar, Window, double>("LeftCaptionButtonsWidth");
+        public static double GetLeftCaptionButtonsWidth(Window window)
+        {
+            return window.GetValue(WindowTitleBar.LeftCaptionButtonsWidthProperty);
+        }
+        internal static void SetLeftCaptionButtonsWidth(Window window, double newLeftCaptionButtonsWidth)
+        {
+            window.SetValue(WindowTitleBar.LeftCaptionButtonsWidthProperty, newLeftCaptionButtonsWidth);
+        }
+        double _leftCaptionButtonsWidth = 0;
+
+
+        public static readonly AttachedProperty<double> RightCaptionButtonsWidthProperty =
+            AvaloniaProperty.RegisterAttached<WindowTitleBar, Window, double>("RightCaptionButtonsWidth");
+        public static double GetRightCaptionButtonsWidth(Window window)
+        {
+            return window.GetValue(WindowTitleBar.RightCaptionButtonsWidthProperty);
+        }
+        internal static void SetRightCaptionButtonsWidth(Window window, double newRightCaptionButtonsWidth)
+        {
+            window.SetValue(WindowTitleBar.RightCaptionButtonsWidthProperty, newRightCaptionButtonsWidth);
+        }
+        double _rightCaptionButtonsWidth = 0;
 
 
         
@@ -96,6 +137,21 @@ namespace AvaloniaPlexTheme
 
         static WindowTitleBar()
         {
+            WindowTitleBar.WindowCaptionButtonsProperty.Changed.AddClassHandler<WindowTitleBar>((s, e) =>
+            {
+                CaptionButtons oldValue = null;
+                CaptionButtons newValue = null;
+                
+                if ((e.OldValue != null) && (e.OldValue is CaptionButtons oldVal))
+                    oldValue = oldVal;
+                
+                if ((e.NewValue != null) && (e.NewValue is CaptionButtons newVal))
+                    newValue = newVal;
+
+                s.OnWindowCaptionButtonsPropertyChanged(oldValue, newValue);
+            });
+
+
             //HeightProperty.OverrideMetadata<WindowTitleBar>(new StyledPropertyMetadata<double>(defaultValue: double.NaN, coerce: CoerceHeight));
 
             //ItemsProperty.Changed.AddClassHandler<WindowTitleBar>(TitlePropertiesChanged);
@@ -108,6 +164,118 @@ namespace AvaloniaPlexTheme
                     sender.Title = e.NewValue.ToString();
                 }
             });
+        }
+
+
+
+        Window _rootWindow = null;
+        /*protected Window RootWindow 
+        {
+            //get => _rootWindow;
+            set
+            {
+                if (value != null)
+                {
+                    //this[!HeightProperty] = value[!Window.ExtendClientAreaTitleBarHeightHintProperty];
+                    //this[!ItemsProperty] = value.GetObservable(TitleProperty).Select(x => (IEnumerable)x).ToBinding();
+                    //this[!UnformattedTitleProperty] = value[!Window.TitleProperty];
+                }
+                
+                _rootWindow = value;
+            }
+        }*/
+
+        /*protected Window RootWindow
+        {
+            get
+            {
+                if ((VisualRoot != null) && (VisualRoot is Window win))
+                    return win;
+                else
+                    return null;
+            }
+        }*/
+
+
+        public virtual void OnWindowCaptionButtonsPropertyChanged(CaptionButtons oldButtons, CaptionButtons newButtons)
+        {
+            if (oldButtons != null)
+                DetachCaptionButtons(oldButtons);
+            
+            if ((newButtons != null) && (_rootWindow != null))
+                AttachCaptionButtons(newButtons);
+        }
+
+        void AttachCaptionButtons(CaptionButtons newButtons)
+        {
+            newButtons.Attach(_rootWindow);
+            newButtons.LayoutUpdated += CaptionButtons_LayoutUpdated;
+            RefreshCaptionButtonWidthProperties(newButtons);
+        }
+
+        void DetachCaptionButtons(CaptionButtons oldButtons)
+        {
+            oldButtons.Detach();
+            oldButtons.LayoutUpdated -= CaptionButtons_LayoutUpdated;
+            RefreshCaptionButtonWidthProperties(null);
+        }
+
+
+
+        void CaptionButtons_LayoutUpdated(object sender, EventArgs e)
+        {
+            if ((_rootWindow != null) && (sender != null) && (sender is CaptionButtons captionButtons))
+                RefreshCaptionButtonWidthProperties(captionButtons);
+            else
+                RefreshCaptionButtonWidthProperties(null);
+        }
+
+        
+        //void RefreshCaptionButtonWidthProperties() => RefreshCaptionButtonWidthProperties(WindowCaptionButtons);
+        void RefreshCaptionButtonWidthProperties(CaptionButtons captionButtons)
+        {
+            if (_rootWindow.ExtendClientAreaToDecorationsHint && (captionButtons != null))
+            {
+                if (DesiredCaptionButtonsAlignment == HorizontalAlignment.Left)
+                {
+                    SetLeftCaptionButtonsWidth(_rootWindow, captionButtons.Bounds.Width);
+                    SetRightCaptionButtonsWidth(_rootWindow, 0);
+                }
+                else
+                {
+                    SetRightCaptionButtonsWidth(_rootWindow, captionButtons.Bounds.Width);
+                    SetLeftCaptionButtonsWidth(_rootWindow, 0);
+                }
+            }
+            else
+            {
+                SetRightCaptionButtonsWidth(_rootWindow, 0);
+                SetLeftCaptionButtonsWidth(_rootWindow, 0);
+            }
+        }
+
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToVisualTree(e);
+            var captionButtons = WindowCaptionButtons;
+
+            if (e.Root is Window window)
+                _rootWindow = window;
+
+            if (captionButtons != null)
+                AttachCaptionButtons(captionButtons);
+        }
+
+        protected override void OnDetachedFromVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromVisualTree(e);
+            var captionButtons = WindowCaptionButtons;
+
+            if (captionButtons != null)
+                DetachCaptionButtons(captionButtons);
+
+            _rootWindow = null;
         }
 
         static void TitlePropertiesChanged(WindowTitleBar sender, AvaloniaPropertyChangedEventArgs args)
@@ -156,23 +324,6 @@ namespace AvaloniaPlexTheme
             RefreshTitleProperties();
         }
 
-        Window? _rootWindow = null;
-        protected Window RootWindow 
-        {
-            get => _rootWindow;
-            set
-            {
-                if (value != null)
-                {
-                    //this[!HeightProperty] = value[!Window.ExtendClientAreaTitleBarHeightHintProperty];
-                    //this[!ItemsProperty] = value.GetObservable(TitleProperty).Select(x => (IEnumerable)x).ToBinding();
-                    //this[!UnformattedTitleProperty] = value[!Window.TitleProperty];
-                }
-                
-                _rootWindow = value;
-            }
-        }
-
 
         /*protected override IItemContainerGenerator CreateItemContainerGenerator()
         {
@@ -198,7 +349,7 @@ namespace AvaloniaPlexTheme
         {
             base.OnApplyTemplate(e);
 
-            e.NameScope.Find<Button>("PART_CloseButton").Click += (s, a) =>
+            /*e.NameScope.Find<Button>("PART_CloseButton").Click += (s, a) =>
             {
                 if (VisualRoot.GetVisualRoot() is Window win)
                     win.Close();
@@ -210,7 +361,7 @@ namespace AvaloniaPlexTheme
             {
                 if (VisualRoot.GetVisualRoot() is Window win)
                     win.WindowState = WindowState.Minimized;
-            };
+            };*/
 
             var dragGrip = e.NameScope.Find<TemplatedControl>("PART_DragGrip");
             
